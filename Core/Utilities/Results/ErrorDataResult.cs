87f5e53a9154efcee6689b3661603092b3446aa1
SDONER <serpil_4984@hotmail.com>
2021-02-16 20:38:37 +0300

    MyFinalProject Update

---
+++ Core/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class ErrorsDataaResult<T> : DataResult<T>
+    {
+        public ErrorsDataaResult(T data, string message) : base(data, false, message)
+        {
+
+        }
+
+        public ErrorsDataaResult(T data) : base(data, false)
+        {
+
+        }
+
+        public ErrorsDataaResult(string message) : base(default, false, message)
+        {
+
+        }
+
+        public ErrorsDataaResult() : base(default, false)
+        {
+
+        }
+    }
+}
