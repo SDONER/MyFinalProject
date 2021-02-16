d360885e812c73475605bed4e8fdab41e40ec7a6
SDONER <serpil_4984@hotmail.com>
2021-02-16 23:34:03 +0300

    MyFinalProject Update

---
+++ WepAPI/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WepAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //Loosley Coupled
+        //IoC -- Inversion of Control
+        IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            {
+                //Dependency chain -- Bağımlılık (Yukarıda Consractor İnjection yapıldı)
+                var result = _productService.GetAll();
+                //swagger
+                if (result.Success)
+                {
+                    return Ok(result.Data);
+                }
+                return BadRequest(result.Message);
+
+            }
+
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(int Id)
+        {
+            var result = _productService.GetById(Id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("Add")]
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
