bf2640eeac5e938a546bc09d8e265dd0d0ce2617
SDONER <serpil_4984@hotmail.com>
2021-02-18 01:01:56 +0300

    MyFinalProject(AOP/CrossCuttingConcern Integration & Test with Postman)

--- WepAPI/Startup.cs
+++ WepAPI/Startup.cs
@@ -32,9 +32,10 @@ namespace WepAPI
             //AOP
             //Autofac, Ninject,CastleWindsor,StructerMap,LightInject,DryInject -->Ioc Container
             //AOP
+            //Postsharp
             services.AddControllers();
-            services.AddSingleton<IProductService , ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService , ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
